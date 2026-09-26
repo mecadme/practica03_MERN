4bb3c988ef0bded5da7c32774179b7a99d486851
Mauro Cadme <mecadme@hotmail.com>
2026-09-26 11:36:33 -0500

    feat: update ecosystem configuration for production deployment settings

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -6,26 +6,34 @@ module.exports = {
       args: 'src/index.ts',
       instances: 'max',
       exec_mode: 'cluster',
-      env: {
+
+      env_production: {
         NODE_ENV: 'production',
         PORT: 3000
       },
+
       error_file: './logs/err.log',
       out_file: './logs/out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       merge_logs: true
     }
   ],
+
   deploy: {
     production: {
       user: 'ubuntu',
       host: '3.151.244.205',
+
+      key: '/home/mcadme/.ssh/claveServer.pem',
+
       ref: 'origin/reto-3',
+
       repo: 'https://github.com/mecadme/practica03_MERN.git',
-      path: '/var/www/empleados-app/practica03_MERN',
+
+      path: '/var/www/empleados-deploy',
+
       'post-deploy':
-        'cd backend && mkdir -p logs && npm install && pm2 startOrReload ecosystem.config.cjs --env production && pm2 save',
-      ssh_options: '/home/mcadme/.ssh/claveServer.pem'
+        'cd backend && mkdir -p logs && npm install && pm2 startOrReload ecosystem.config.cjs --env production && pm2 save'
     }
   }
 };
